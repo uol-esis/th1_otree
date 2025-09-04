b45379934cd0ec83eaed871bb9fde77578efa472
k47h0b <katharina.hobbensiefken@uni-oldenburg.de>
2025-09-04 11:00:51 +0200

    changed the form_fields in the post_questionnaire for Treatment A and B

--- th1nk_app/pages.py
+++ th1nk_app/pages.py
@@ -151,43 +151,6 @@ class TreatmentB_difficult(Page):
             if getattr(self.player, field) == should_be_true:
                 score += 1
         self.player.score += score
-    
-    
-
-    '''
-    def vars_for_template(self):
-        return {
-            'external_url': f'https://yourtool.com/start?participant={self.participant.code}'
-        }
-
-    def post(self):
-        uploads_dir = os.path.join(os.getcwd(), 'uploads')
-        if not os.path.exists(uploads_dir):
-            os.makedirs(uploads_dir)
-
-        uploaded_file = self.request.POSTFILES.get('upload')
-        if uploaded_file:
-            filename = f"{self.participant.code}_{uploaded_file.name}"
-            path = os.path.join('uploads', filename)
-            default_storage.save(path, uploaded_file)
-            self.player.uploaded_filename = filename
-        return super().post()
-
-    def before_next_page(self, timeout_happened):
-        # Automatische Auswertung Multiple Choice
-        correct_answers = {'mc1': 'A', 'mc2': 'B', 'mc3': 'C'}
-        score = sum(getattr(player, q) == ans for q, ans in correct_answers.items())
-        self.score_mc = score
-
-        # Beispiel für offene Fragen – hier nur Länge der Antwort > 0 als "korrekt"
-        open_score = sum(len(getattr(self, q) or '') > 0 for q in ['open1', 'open2', 'open3'])
-        self.score_open = open_score
-
-        if timeout_happened:
-            self.participant.vars['timeout_reached'] = True
-        else:
-            self.participant.vars['timeout_reached'] = False
-'''
 
 class ResultDifficultA(Page):
     form_model = 'player'
@@ -217,7 +180,12 @@ class TimeUp(Page):
 
 class PostQuestionnaire(Page):
     form_model = 'player'
-    form_fields = ['post_experience', 'difficulties', 'satisfaction']
+
+    def get_form_fields(self):
+        if self.player.treatment == 'A':
+            return ['post_experience', 'difficulties', 'satisfaction']
+        else:
+            return ['difficulties', 'satisfaction']
 
 class ThankYou(Page):
     form_model = 'player'
