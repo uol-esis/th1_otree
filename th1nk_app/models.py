e0591fc4ee77dc32db243d86e044e34eac317861
k47h0b <katharina.hobbensiefken@uni-oldenburg.de>
2025-10-01 09:26:40 +0200

    updated completion link

--- th1nk_app/models.py
+++ th1nk_app/models.py
@@ -42,7 +42,7 @@ class Subsession(BaseSubsession):
         #A = Tool, B = Excel
         for p in self.get_players():
             #p.treatment = random.choice(C.TREATMENT)
-            p.treatment = 'A'
+            p.treatment = 'B'
 
 class Group(BaseGroup):
     pass
